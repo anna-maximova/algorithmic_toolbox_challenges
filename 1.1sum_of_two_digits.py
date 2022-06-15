332dbb01f8d82abb6061911034408bb2960840e8
Anna Maximova <annamaximova3418@gmail.com>
2022-06-14 21:25:19 -0700

    Commit week 1 challenge 1

---
+++ 1.1sum_of_two_digits.py
@@ -0,0 +1,5 @@
+# Program objective: read two digits and print their sum
+
+inputs = input("Enter two digits with a space in between: ")
+
+print("The sum is", int(inputs.split()[0]) + int(inputs.split()[1]))
